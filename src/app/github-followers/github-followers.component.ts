f2b42c030508bc099680631b0a6ba341909a8fba
Marina Rumyansev <aniramrum@gmail.com>
2018-11-01 18:10:33 +0200

    Initial commit

---
+++ src/app/github-followers/github-followers.component.ts
@@ -0,0 +1,58 @@
+import { GithubFollowersService } from './../services/github-followers.service';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/combineLatest';
+
+@Component({
+  selector: 'github-followers',
+  templateUrl: './github-followers.component.html',
+  styleUrls: ['./github-followers.component.css']
+})
+export class GithubFollowersComponent implements OnInit {
+  followers: any[];
+
+  constructor(
+    private route: ActivatedRoute,
+    private service: GithubFollowersService) { }
+
+  ngOnInit() {
+
+    // subscribing different observables same time
+    let obs = Observable.combineLatest([
+      this.route.paramMap,  // required params [0]
+      this.route.queryParamMap  // query params
+    ])
+    .subscribe(combined => {
+      let id = combined[0].get('id'); // getting data from required params
+      let page = combined[1].get('page'); // getting data from query params
+
+      // this.service.getAll({ id: id, page: page })
+
+      this.service.getAll()
+        .subscribe(followers => this.followers = followers);
+    });
+
+    // this.route.queryParamMap
+    //   .subscribe(params => {
+
+    // });
+
+    // this.route.paramMap
+    // .subscribe(params => {
+
+    // });
+    // -------
+    // // getting required params
+    // this.route.paramMap.subscribe();
+    // // or
+    // // let id = this.route.snapshot.paramMap.get('id');
+
+    // // getting query params (optional params)
+    // this.route.queryParamMap.subscribe();
+    // // or
+    // // let page = this.route.snapshot.queryParamMap.get('id');
+
+    
+  }
+}
---
+++ src/app/github-profile/github-profile.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-github-profile',
+  templateUrl: './github-profile.component.html',
+  styleUrls: ['./github-profile.component.css']
+})
+export class GithubProfileComponent implements OnInit {
+
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    // if we 100% sure we won't need to change parameters while staying in the same component
+
+    // let id = this.route.snapshot.paramMap.get('id');
+    
+    // instead of using observables and subscribing to events
+    // then A will every time destroy the component when leaving page and recreate it when load it again
+    // with new parameters
+
+    this.route.paramMap
+      .subscribe(params => {
+        // console.log(params);
+        let id = +params.get('id');
+
+        console.log(id);
+      });
+  }
+
+}
